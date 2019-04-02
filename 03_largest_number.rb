d671aef257f4f69a02ebb499d937df9e6ab0050e
jeffdorling <jdorling15@gmail.com>
2019-04-03 09:21:14 +1000

    added comments

---
+++ 03_largest_number.rb
@@ -0,0 +1,37 @@
+# Write a method that will take two numbers,
+# and return whichever is the largest.
+#
+# Example:
+# largest_number(100,50) should return 100
+# largest_number(10,20) should return 20
+#
+# Check your solution by filling in the folowing table:
+# number_1 || number_2 || expected || actual
+# 
+# If your stuck try working togeather
+# 
+# Beast Mode:
+# If you complete the challenge, feel free to come up
+# with your own examples and test corner cases.
+# 
+# Examples - what if both numbers are equal or are strings?
+
+def largest_number (num1, num2)
+  puts "Enter your first number"
+  num1 = gets.chomp
+  puts "Enter your second number"
+  num2 = gets.chomp
+  if 
+    #If both numbers are equal
+    num1 == num2
+    puts "The numbers are equal"
+    elsif
+      #Finds the largest number by asking if the 2nd number
+      #is larger then first. 
+    num1 < num2
+    puts "The largest number is #{num2}"
+    else
+    puts "The largest number is #{num1}"   
+end
+end
+largest_number(100,50)
